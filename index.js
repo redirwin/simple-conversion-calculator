a808dc1dbd2a7a259e7cd6ad6f5c2c662c5bce4c
Dave Irwin <david@redirwin.com>
2022-05-15 21:23:52 -0700

    Attempting to get user input.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let inputNum = 20;
+let inputNum = 7;
 
-document.getElementById("input").textContent = inputNum;
+document.getElementById("input").textContent = inputNum; // Changes input to hard coded input.
+
 document.getElementById("input-meters").textContent = inputNum;
 document.getElementById("input-feet").textContent = inputNum;
 document.getElementById("input-liters").textContent = inputNum;
@@ -10,10 +11,10 @@ document.getElementById("input-pounds").textContent = inputNum;
 
 let metersToFeet = (inputNum * 3.28084).toFixed(3);
 let feetToMeters = (inputNum * 0.3048).toFixed(3);
-let litersToGallons = (inputNum * .264172).toFixed(3);
+let litersToGallons = (inputNum * 0.264172).toFixed(3);
 let gallonsToLiters = (inputNum * 3.78541).toFixed(3);
 let kilosToPounds = (inputNum * 2.20462).toFixed(3);
-let poundsToKilos = (inputNum * .453592).toFixed(3);
+let poundsToKilos = (inputNum * 0.453592).toFixed(3);
 
 document.getElementById("meters-to-feet").textContent = metersToFeet;
 document.getElementById("feet-to-meters").textContent = feetToMeters;
